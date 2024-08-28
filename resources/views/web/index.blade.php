2e61b866dc2b7acf23f108a99bf4fc907278cfc8
nuttttx <rakuzanoat@gmail.com>
2024-08-29 00:18:52 +0700

    Add new layout.

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.my-app')
+@section('content')
+    <div class="p-4">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My shop</title>
-</head>
+        <ul>
+            <li>ที่อยู่ร้าน: {{ $address }}</li>
+            <li>เบอร์โทร: {{ $mobilePhone }}</li>
+            <li>ที่ตั้งร้าน: {{ $location }}</li>
+        </ul>
+        <h2>Users list</h2>
 
-<body>
-    <h1>ITShop</h1>
-    <h2>Shop information</h2>
-    <ul>
-        <li>ที่อยู่ร้าน: {{$address}}</li>
-        <li>เบอร์โทร: {{$mobilePhone}}</li>
-        <li>ที่ตั้งร้าน: {{$location}}</li>
-    </ul>
-    <h2>Users list</h2>
-
-    <ul>
-        @forelse ($users as $user)
-            <li>{{ $user->name }} {{ "<$user->email>" }}</li>
-        @empty
-            <p>No users</p>
-        @endforelse
-    </ul>
-</body>
-
-</html>
+        <ul>
+            @forelse ($users as $user)
+                <li>{{ $user->name }} {{ "<$user->email>" }}</li>
+            @empty
+                <p>No users</p>
+            @endforelse
+        </ul>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/myshop', [WebController::class, 'webPage'])->name("web.myshop");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,6 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/myshop', [WebController::class, 'webPage'])->name("web.myshop");
 });
 
 require __DIR__.'/auth.php';
